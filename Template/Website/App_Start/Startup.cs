4947361eb934e6040719b8a0bdee4210782f5571
PaymonK <35069374+PaymonK@users.noreply.github.com>
2021-05-11 09:37:09 +0100

    Update Startup.cs

--- Template/Website/App_Start/Startup.cs
+++ Template/Website/App_Start/Startup.cs
@@ -3,11 +3,14 @@ namespace Website
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.Logging;
+    using Olive.Aws;
 
     public abstract class Startup : FS.Shared.Website.Startup<Domain.ReferenceData, Domain.BackgroundTask, TaskManager>
     {
         protected Startup(IWebHostEnvironment env, IConfiguration config, ILoggerFactory factory) : base(env, config, factory)
         {
         }
+        
+        protected override SecretProviderType GetSecretsProvider() => SecretProviderType.SystemsManagerParameter;
     }
 }
