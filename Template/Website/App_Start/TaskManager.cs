eb183a528a2e93044f4c153db3110d2a93a5a7ab
ali_akhavanrad_geeksltd <ali.akhavanrad@geeks.ltd.uk>
2022-06-21 16:32:14 +0100

    Update auto-generated files

--- Template/Website/App_Start/TaskManager.cs
+++ Template/Website/App_Start/TaskManager.cs
@@ -10,23 +10,32 @@
     using Olive.Entities;
     using Olive.Entities.Data;
     using Domain;
-    
+
     /// <summary>Executes the scheduled tasks in independent threads automatically.</summary>
     [EscapeGCop("Auto generated code.")]
-    #pragma warning disable
+#pragma warning disable
     public partial class TaskManager : BackgroundJobsPlan
     {
         /// <summary>Registers the scheduled activities.</summary>
         public override void Initialize()
         {
-            Register(new BackgroundJob("Clean old temp uploads", () => CleanOldTempUploads(), Hangfire.Cron.MinuteInterval(10)));
+            Register(new BackgroundJob("Sample task", () => SampleTask(), Hangfire.Cron.HourInterval(1)));
         }
-        
-        /// <summary>Clean old temp uploads</summary>
-        public static async Task CleanOldTempUploads()
+
+        /// <summary>Sample task</summary>
+        public static Task SampleTask()
         {
-            await Olive.Context.Current.GetService<Olive.Mvc.IFileRequestService>()
-                .DeleteTempFiles(olderThan: 1.Hours());
+            try
+            {
+                //int i=0;
+            }
+            catch (Exception ex)
+            {
+                Log.For<TaskManager>().Error(ex);
+                throw;
+            }
+
+            return Task.CompletedTask;
         }
     }
 }
